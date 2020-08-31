b6a3db48ef09b96c4e8e725b5f0941e383697b97
Valerio <Valerio@192.168.1.188>
2020-08-31 20:15:16 +0200

    creazione dao

--- src/main/java/it/polito/tdp/food/model/Model.java
+++ src/main/java/it/polito/tdp/food/model/Model.java
@@ -1,5 +1,30 @@
 package it.polito.tdp.food.model;
 
+import it.polito.tdp.food.db.FoodDao;
+
 public class Model {
+	
+	
+	
+private FoodDao dao;
+
+
+
+
+
+
+
+
+public Model() {
+	dao= new FoodDao();
+}
+
+
+
+
+
+
+
+
 
 }
